340bca1b70d18cbe19184770ea2de07b536b2370
AnaliaMagallanes <167925588+AnaliaMagallanes@users.noreply.github.com>
2024-12-08 20:09:33 +0000

    creacion de cartas aleatoreas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "k"
+  ];
+
+  let symbols = ["♧", "♡", "♤", "♦"];
+
+  let colors = ["red", "black"];
+
+  let cardSymbolHeader = document.getElementById("card-symbol-header");
+
+  let cardSymbolValue = document.getElementById("card-value");
+
+  let cardSymbolFooter = document.getElementById("card-symbol-footer");
+
+  let randomValues = Math.floor(Math.random() * values.length);
+  let randomSymbols = Math.floor(Math.random() * symbols.length);
+  let randomColors = Math.floor(Math.random() * colors.length);
+
+  cardSymbolHeader.innerHTML = `<p class='${colors[randomColors]}'> ${symbols[randomSymbols]} </p>`;
+
+  cardSymbolValue.innerHTML = `<p class='${values[randomValues]}'> ${values[randomValues]}</p>`;
+
+  cardSymbolFooter.innerHTML = `<p class='${colors[randomColors]}'> ${symbols[randomSymbols]} </p>`;
 };
